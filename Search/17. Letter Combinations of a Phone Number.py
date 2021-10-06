58e772b95cbe9584e35013a4b96a2de2e8f34426
ClarkBrun <33387007+ClarkBrun@users.noreply.github.com>
2021-10-06 17:55:33 +0200

    Update 17. Letter Combinations of a Phone Number.py

--- Search/17. Letter Combinations of a Phone Number.py
+++ Search/17. Letter Combinations of a Phone Number.py
@@ -5,23 +5,35 @@ class Solution(object):
         :rtype: List[str]
         """
         digitsdict = {
-            2 = ['a', 'b', 'c'],
-            3 = ['d', 'e', 'f'],
-            4 = ['g', 'h', 'i'],
-            5 = ['j', 'k', 'l'],
-            6 = ['m', 'n', 'o'],
-            7 = ['p', 'q', 'r', 's'],
-            8 = ['t', 'u', 'v'],
-            9 = ['w', 'x', 'y', 'z']
+            '2' : ['a', 'b', 'c'],
+            '3' : ['d', 'e', 'f'],
+            '4' : ['g', 'h', 'i'],
+            '5' : ['j', 'k', 'l'],
+            '6' : ['m', 'n', 'o'],
+            '7' : ['p', 'q', 'r', 's'],
+            '8' : ['t', 'u', 'v'],
+            '9' : ['w', 'x', 'y', 'z']
         }
         
-        integer = []
+        # integer = []
         strings = []
+        output = []
 
         if len(digits) > 0:
-            for i in len(digits):
-                integer.append(digits[i])
+            for i in range(len(digits)):
+                # integer.append(digits[i])
                 strings.append(digitsdict[digits[i]])
+
+            while(len(strings) > 1):
+                for j in range(len(strings[0])):
+                    for k in range(len(strings[1])):
+                        output.append(strings[0][j] + strings[1][k])
+                
+                strings.pop(0)
+                strings[0] = output
+                output = []
+
+            return strings[0]
 
         else:
             return []
